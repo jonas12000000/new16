06d978d20be15a4bee23a47190916b8c997f007c
Jonas <jonas12000000@gmail.com>
2025-12-24 14:17:31 -0500

    add home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,54 @@
+import type { Metadata } from "next";
+import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
+import "./globals.css";
+import LightRays from "@/components/ui/LightRays";
+import Navbar from "@/components/Navbar";
 
 
-const Layout = () => {
+const schibstedGrotesk = Schibsted_Grotesk({
+  variable: "--font-schibsted-grotesk",
+  subsets: ["latin"],
+});
+
+
+const martianMono = Martian_Mono({
+  variable: "--font-martian-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DevEvent",
+  description: "The hub to every dev events, you can't miss",
+};
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <div>layout</div>
-  )
+    <html lang="en">
+      <body
+        className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
+      >
+        <Navbar />
+        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfeca"
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.02}
+            noiseAmount={0.0}
+            distortion={0.01}
+          />
+        </div>
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
 }
-
-export default Layout
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import EventCard from '@/components/EventCard'
+import ExploreBtn from '@/components/ExploreBtn'
+import { events } from '@/lib/constants'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section>
+      <h1 className='text-center'>All the dev Events <br/> You Can't Miss OUT.</h1>
+      <p className='text-center mt-5'>Hackatons, Meetups, and Conferences, ALL IN ONE PLACE</p>
+      <ExploreBtn />
+
+      <div className='mt-20 space-y-7'>
+        <h3>Featured Events</h3>
+        <ul className='events'>
+          {events.map((event) => (
+            <li key={event.slug}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   )
 }
 
